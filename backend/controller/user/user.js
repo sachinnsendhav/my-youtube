b8ec9392118ebdd8cd02eb6524adb37ee3eb9431
AmberDube <amber.dube@codesquaretech.com>
2024-01-03 17:12:55 +0530

    changes in signup

--- backend/controller/user/user.js
+++ backend/controller/user/user.js
@@ -1,13 +1,25 @@
 const User = require('../../model/user/userModel');
+const jwt=require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+function issueJwt(paylod){
+    const token=jwt.sign({paylod},`my-youtube`,{expiresIn:'1h'})
+    return token
+}
 
 exports.signup = async(req,res) => {
     try{
-        const { firstName, lastName, email, phoneNumber, password, userRole } = req.body;
+        const { firstName, lastName, email, phoneNumber, userPassword, userRole } = req.body;
+        let userExist=await User.findeOne({email})
+        if (userExist) {
+         res.status(400).send( { status:400, message:"User already exists" } );
+        }
         const role = userRole || 'user';
-        const payload = { firstName, lastName, email, phoneNumber, password, role };
+        const password=bcrypt.hash( userPassword,10)
+        const payload = { firstName, lastName, email, phoneNumber,password, role };
         const user = new User(payload);
         await user.save();
-        res.status(200).send( { status:200, message:"User registered successfully" } );
+        const token=issueJwt(payload)
+        res.status(200).send( { status:200, message:"User registered successfully",token } );
     }
     catch(error){
         return res.status(400).send( { status:400, message:error.message } );
