5da38bf0c9ba0c48529a1504ec1ae7de5ccfe480
Danish-CodeSquare <mohammad.danish@codesquaretech.com>
2024-01-03 17:21:33 +0530

    Add jwt token and bcrypt password for signup

--- backend/controller/user/user.js
+++ backend/controller/user/user.js
@@ -1,6 +1,6 @@
 const User = require('../../model/user/userModel');
 const jwt=require('jsonwebtoken')
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 function issueJwt(paylod){
     const token=jwt.sign({paylod},`my-youtube`,{expiresIn:'1h'})
     return token
@@ -9,17 +9,17 @@ function issueJwt(paylod){
 exports.signup = async(req,res) => {
     try{
         const { firstName, lastName, email, phoneNumber, userPassword, userRole } = req.body;
-        let userExist=await User.findeOne({email})
+        let userExist = await User.findOne({email})
         if (userExist) {
-         res.status(400).send( { status:400, message:"User already exists" } );
+            return res.status(400).send( { status:400, message:"User already exists" } );
         }
         const role = userRole || 'user';
-        const password=bcrypt.hash( userPassword,10)
+        const password=await bcrypt.hash(userPassword,10);
         const payload = { firstName, lastName, email, phoneNumber,password, role };
         const user = new User(payload);
         await user.save();
         const token=issueJwt(payload)
-        res.status(200).send( { status:200, message:"User registered successfully",token } );
+        return res.status(200).send( { status:200, message:"User registered successfully",token } );
     }
     catch(error){
         return res.status(400).send( { status:400, message:error.message } );
