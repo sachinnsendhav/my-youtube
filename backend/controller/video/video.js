71978d64b9c54e118b33983333dcedf54aadf54b
Danish-CodeSquare <mohammad.danish@codesquaretech.com>
2024-01-08 18:56:17 +0530

    Update the video api with userId and playListApi

--- backend/controller/video/video.js
+++ backend/controller/video/video.js
@@ -1,33 +1,33 @@
 const videoDetails = require("../../model/user/videoModel");
- module.exports={
 
-video:async(req,res) => {
-    console.log(req.user.paylod,"auth req user");
-    try{
-        const role = req.user.paylod.role;
-        if(role == 'user'){
-           return  res.status(401).send( { status:401, message:"Not Authorized" } );
+module.exports={
+    video:async(req,res) => {
+        console.log(req.user.paylod,"auth req user");
+        try{
+            const role = req.user.paylod.role;
+            if(role == 'user'){
+            return  res.status(401).send( { status:401, message:"Not Authorized" } );
+            }
+            const { videoName, videoDescription, videoUrl, playListId} = req.body;
+            const payload = {  videoName, videoDescription, videoUrl, playListId, userId:req.user.paylod._id };
+            const video = new videoDetails(payload);
+            await video.save();
+            res.status(200).send( { status:200, message:"Video details has been added successfully" } );
         }
-        const { videoName, videoDescription, videoUrl, category} = req.body;
-        const payload = {  videoName, videoDescription, videoUrl, category };
-        const video = new videoDetails(payload);
-        await video.save();
-        res.status(200).send( { status:200, message:"Video details has been added successfully" } );
-    }
-    catch(error){
-        console.log("err",error)
-        return res.status(400).send( { status:400, message:error.message } );
+        catch(error){
+            console.log("err",error)
+            return res.status(400).send( { status:400, message:error.message } );
 
+        }
+    },
+    getVideo:async(req,res,next)=>{
+        try {
+            const playListId = req.params.playListId;
+            const videos = await videoDetails.find({playListId:playListId});
+            res.status(200).send({ status: 200, data: videos });
+        } catch (error) {
+            console.log("err", error);
+            return res.status(400).send({ status: 400, message: error.message });
+        }
     }
-},
-getVideo:async(req,res,next)=>{
-    try {
-        const videos = await videoDetails.find();
-        console.log(videos);
-        res.status(200).send({ status: 200, data: videos });
-    } catch (error) {
-        console.log("err", error);
-        return res.status(400).send({ status: 400, message: error.message });
-    }
-}
 }
--- backend/routes/video/videoRoutes.js
+++ backend/routes/video/videoRoutes.js
@@ -4,5 +4,5 @@ const videoController = require('../../controller/video/video');
 const auth = require('../../middleware/auth');
 
 videorouter.post('/video/uploadData',auth, videoController.video);
-videorouter.get('/video/getData', videoController.getVideo);
+videorouter.get('/video/getData/:playListId', videoController.getVideo);
 module.exports = videorouter;
