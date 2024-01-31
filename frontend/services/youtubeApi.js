5598b74753f219dca86a552f60c2ff6e71132ec5
sachinnsendhav <sachin.sendhav@codesquaretech.com>
2024-01-31 16:18:20 +0530

    add headers in mobile app

--- frontend/services/youtubeApi.js
+++ frontend/services/youtubeApi.js
@@ -6,8 +6,9 @@ const options = {
         maxResults: 50,
     },
     headers: {
-        'X-RapidAPI-Key': '9f64104e16msh5b7288224bca0b2p11551cjsnb71033460e12',
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+        headers: {
+            "Accept": "application/json"
+        },
     },
 };
 const getVideosBySearch = (search) =>
