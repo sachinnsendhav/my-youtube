6afccfb9b573613e055b9925450f0ebcbd41c0ba
Divyansh-Csqt <divyansh.bajpai@codesquaretech.com>
2024-02-02 02:58:15 +0530

    fix api endpoint

--- frontend/screens/ProfileScreen.js
+++ frontend/screens/ProfileScreen.js
@@ -65,7 +65,7 @@ const ProfileScreen = () => {
 
   const fetchDefaultAvatar = async () => {
     try {
-      const getUserApiUrl = `http://173.214.174.234/api/getUserType/${userObjectId}`;
+      const getUserApiUrl = `http://173.214.174.234:3005/api/getUserType/${userObjectId}`;
       const response = await fetch(getUserApiUrl);
   
       if (!response.ok) {
